f602749b4b07da40507259f9b9831a3e078b408a
Sam K <113443285+skettlewell3@users.noreply.github.com>
2025-12-30 16:34:18 +0000

    feat: Fixed js logic
    Redesigned the js logic to better handle variables to be rendered in Display

--- src/utils/time.js
+++ src/utils/time.js
@@ -11,14 +11,23 @@ export function calculatePausedTime(session) {
 }
 
 export function formatTime(ms) {
+    if (!ms || ms < 0) ms = 0;
     const totalSeconds = Math.floor(ms / 1000);
     const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor(totalSeconds % 3600) / 60;
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
 
     const hh = hours.toString().padStart(2, '0');
     const mm = minutes.toString().padStart(2, '0');
     const ss = seconds.toString().padStart(2, '0');
+
+    return `${hh}:${mm}:${ss}`;
+}
+
+export function formatSystemTime(date = new Date()) {
+    const hh = date.getHours().toString().padStart(2, '0');
+    const mm = date.getMinutes().toString().padStart(2, '0');
+    const ss = date.getSeconds().toString().padStart(2, '0');
 
     return `${hh}:${mm}:${ss}`;
 }
